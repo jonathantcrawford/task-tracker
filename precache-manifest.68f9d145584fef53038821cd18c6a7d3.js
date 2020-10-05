062acbea6c003781d7e87ef725b0f225e1d85ac5
jonathantcrawford <jon_t_crawford@protonmail.com>
2020-10-04 22:44:09 -0400

    Updates

---
+++ precache-manifest.68f9d145584fef53038821cd18c6a7d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c6e357fe4463c05e6d5c4f88b44e93",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "8fd4387aec963c581419",
+    "url": "/task-tracker/static/css/main.db3eb049.chunk.css"
+  },
+  {
+    "revision": "9543f1ce3f2c9275a411",
+    "url": "/task-tracker/static/js/2.1b82eee1.chunk.js"
+  },
+  {
+    "revision": "d3fc611ef7f627ba2af4ba9baec10348",
+    "url": "/task-tracker/static/js/2.1b82eee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd4387aec963c581419",
+    "url": "/task-tracker/static/js/main.753fa016.chunk.js"
+  },
+  {
+    "revision": "842ce66a363b3f36142e",
+    "url": "/task-tracker/static/js/runtime-main.739e8dac.js"
+  }
+]);
